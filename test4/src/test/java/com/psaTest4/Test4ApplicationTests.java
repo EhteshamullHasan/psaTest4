950ca2ab8c8e730a9b06f8426f23b1aaa4f4c738
hasan <ehteshamullhasan@gmail.com>
2025-03-16 12:06:16 +0530

    first commit

---
+++ test4/src/test/java/com/psaTest4/Test4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psaTest4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
